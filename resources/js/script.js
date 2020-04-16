6beb69312031d7ec177d0c3c90414ce861b707f7
ahmedali8 <imahmedalibhatti@gmail.com>
2020-04-16 12:33:03 +0500

    Scrolling to elements  -added smooth scrolling script in jQuery
    Adding animations on scroll
     -added animate.css
    
    Making the navigation responsive
     -added Mobile nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+
+    /* For the sticky navigation */
     $('.js--section-features').waypoint(function(direction) {
         if(direction == 'down') {
             $('nav').addClass('sticky');
@@ -8,19 +10,90 @@ $(document).ready(function() {
             $('nav').removeClass('sticky');
         }
     }, {
-        offset: '60px;'
+        offset: '60px'
     });
 
 
+    /* Scroll on Buttons */
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
 
+    $('.js--scroll-to-start').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+
+    //For "Back to top"
+    $('.js--scroll-to-top').click(function() {
+        $('html, body').animate({scrollTop: $('.js--top').offset().top}, 1500);
+    });
+
+
+    /* Navigation scroll */
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+    });
+
+
+    /* Animations on Scroll */
+
+    //section-features
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    //app-screen
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    //section-cities
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    //section-plans:first-child (premium)
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
     
-    /**
-        var waypoints = $('#handler-first').waypoint(function(direction) {
-        notify(this.element.id + ' hit 25% from top of window') 
-        }, {
-        offset: '25%'
-        })
-     */
+
+    /* Mobile nav */
+
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon');
+
+        nav.slideToggle(200);
+
+        if(icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        }
+        else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
 
 
 });
